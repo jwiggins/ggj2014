3f7b875744ecf9daec9b4c901411a6a556dedb91
John Wiggins <jwiggins@enthought.com>
2014-01-25 10:53:55 -0600

    Arbitrary character movement!

--- mazeproto.love/character.lua
+++ mazeproto.love/character.lua
@@ -1,18 +1,17 @@
--- Private movement functions
-local function upFunc(self)
-    self.y = self.y - 1
-end
-local function downFunc(self)
-    self.y = self.y + 1
-end
-local function leftFunc(self)
-    self.x = self.x - 1
-end
-local function rightFunc(self)
-    self.x = self.x + 1
+-- Private functions
+local function clampDirection(direction)
+    local circle = math.pi*2.0
+    if direction > circle then
+        return direction - circle
+    elseif direction < 0.0 then
+        return direction + circle
+    end
+    return direction
 end
 
-local moveFuctions = {upFunc, downFunc, leftFunc, rightFunc}
+local function movementVector(self)
+    return self.speed * math.cos(self.facing), self.speed * math.sin(self.facing)
+end
 
 local function randomInRange(start, stop)
     local size = stop-start
@@ -27,11 +26,15 @@ setmetatable(Character, {
     __call = function (cls, ...) return cls.new(...) end
 })
 
+local movementFunctions = {'forward', 'backward', 'rotateLeft', 'rotateRight'}
+
 function Character.new(tab)
     local self = setmetatable({}, Character)
     local width, height = love.window.getDimensions()
     self.x = (width / 2) + randomInRange(-100, 100)
     self.y = (height / 2) + randomInRange(-100, 100)
+    self.facing = math.pi / 2.0
+    self.speed = 1.0
     self.keys = tab.keys
     self.color = tab.color
     return self
@@ -41,16 +44,40 @@ function Character:update()
     local keys = self.keys
     for index, code in pairs(keys) do
         if love.keyboard.isDown(code) then
-            moveFuctions[index](self)
+            Character[movementFunctions[index]](self)
         end
     end
 end
 
 function Character:draw()
-    local x = self.x
-    local y = self.y
+    love.graphics.push()
     love.graphics.setColor(self.color)
-    love.graphics.rectangle("fill", x-10, y-10, 20, 20)
+
+    love.graphics.translate(self.x, self.y)
+    love.graphics.rotate(-self.facing)
+    love.graphics.scale(10.0, 10.0)
+    love.graphics.polygon("fill", 1.0, 0.0, -0.5, 0.866025, -0.5, -0.866025)
+    love.graphics.pop()
+end
+
+function Character:forward()
+    local dX, dY = movementVector(self)
+    self.x = self.x + dX
+    self.y = self.y - dY
+end
+
+function Character:backward()
+    local dX, dY = movementVector(self)
+    self.x = self.x - dX
+    self.y = self.y + dY
+end
+
+function Character:rotateLeft()
+    self.facing = clampDirection(self.facing + math.pi/128.0)
+end
+
+function Character:rotateRight()
+    self.facing = clampDirection(self.facing - math.pi/128.0)
 end
 
 
