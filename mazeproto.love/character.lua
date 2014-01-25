3c143a7ff48d3b4312e90aac2eecfe756036a75d
John Wiggins <jwiggins@enthought.com>
2014-01-25 09:20:08 -0600

    Make a Character class a little more python-like

--- mazeproto.love/character.lua
+++ mazeproto.love/character.lua
@@ -1,15 +1,15 @@
 -- Private movement functions
-local function upFunc(data)
-    data.y = data.y - 1
+local function upFunc(self)
+    self.y = self.y - 1
 end
-local function downFunc(data)
-    data.y = data.y + 1
+local function downFunc(self)
+    self.y = self.y + 1
 end
-local function leftFunc(data)
-    data.x = data.x - 1
+local function leftFunc(self)
+    self.x = self.x - 1
 end
-local function rightFunc(data)
-    data.x = data.x + 1
+local function rightFunc(self)
+    self.x = self.x + 1
 end
 
 local moveFuctions = {upFunc, downFunc, leftFunc, rightFunc}
@@ -22,26 +22,28 @@ setmetatable(Character, {
     __call = function (cls, ...) return cls.new(...) end
 })
 
-function Character.new(init)
+function Character.new(tab)
     local self = setmetatable({}, Character)
-    self.data = init
+    self.x = tab.x
+    self.y = tab.y
+    self.keys = tab.keys
+    self.color = tab.color
     return self
 end
 
 function Character:update()
-    local keys = self.data.keys
+    local keys = self.keys
     for index, code in pairs(keys) do
         if love.keyboard.isDown(code) then
-            moveFuctions[index](self.data)
+            moveFuctions[index](self)
         end
     end
 end
 
 function Character:draw()
-    local data = self.data
-    local x = data.x
-    local y = data.y
-    love.graphics.setColor(data.color)
+    local x = self.x
+    local y = self.y
+    love.graphics.setColor(self.color)
     love.graphics.rectangle("fill", x-10, y-10, 20, 20)
 end
 
